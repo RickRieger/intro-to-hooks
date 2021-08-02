a4e5f5c3fd27fbff4ceb57ec0dfcb43d573f74eb
Rick Rieger <rickrieger36251@gmail.com>
2021-08-02 13:33:09 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+function CustomHooksUsingObjectFormat(initialState) {
+  const [value, setValue] = useState(initialState);
+  function onChange(e) {
+    setValue(e.target.value);
+  }
+  return { value, onChange };
+}
+
 function App() {
+  const firstName = CustomHooksUsingObjectFormat('');
+  const lastName = CustomHooksUsingObjectFormat('');
+  const email = CustomHooksUsingObjectFormat('');
+  const phone = CustomHooksUsingObjectFormat('');
+
+  function handleOnSubmit(e) {
+    e.preventDefault();
+    console.log(firstName.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form onSubmit={handleOnSubmit}>
+        <input type='text' placeholder='first name' {...firstName} />
+        <br />
+        <input type='text' placeholder='last name' {...lastName} />
+        <br />
+        <input type='email' placeholder='email' {...email} />
+        <br />
+        <input type='tel' placeholder='enter contact number' {...phone} />
+        <br />
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   );
 }
-
 export default App;
